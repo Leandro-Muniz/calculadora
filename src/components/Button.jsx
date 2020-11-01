ef150cbda0cbf618961412c88b3d6f43fca103b2
Bexus Soluções Digitais <leandro@bexus.com.br>
2020-11-01 11:29:16 -0300

    Conclusão do projeto

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -10,7 +10,7 @@ export default props => {
 
     return (
         <button 
-            onClick={e =>props.click && props.click(e.target.label)}
+            onClick={e =>props.click && props.click(props.label)}
             className={classes}>
             {props.label}
         </button>
